04f5ad73b538c65cd7bb93b6d13faf7041bea521
Song-Tianxiang <149415622+Song-Tianxiang@users.noreply.github.com>
2024-09-26 21:01:02 +0800

    Update init.lua

--- lua/base46/init.lua
+++ lua/base46/init.lua
@@ -1,13 +1,7 @@
 local M = {}
 
-M.get_theme_tb = function(name, type)
-  local present, theme = pcall(require, "themes." .. name)
+M.override_theme = function(default_theme, theme_name)
 
-  if present then
-    return theme[type]
-  else
-    error "No such theme!"
-  end
 end
 
 return M
